3f44eb616f58166217f433ed76aa68590ade4e9a
Aditya-Shr <46041050+Aditya-Shr@users.noreply.github.com>
2023-01-03 21:09:39 -0500

    Updated file

--- src/com/springdemo/TrackCoach.java
+++ src/com/springdemo/TrackCoach.java
@@ -25,6 +25,16 @@ public class TrackCoach implements Coach {
 		// TODO Auto-generated method stub
 		return "Just Do it:" +fortuneService.getFortune();
 	}
+
+	// add an init method
+	public void doMyStartupStuff(){
+		System.out.println("Trackcoach: inside method domystartupstuff");
+	}
+
+	//add a destroy method
+	public void doMyStartupStuffYoYo(){
+		System.out.println("Trackcoach: inside method domystartupstuffyoyo");
+	}
 	
 	
 
